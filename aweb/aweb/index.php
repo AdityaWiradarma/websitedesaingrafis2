7efb273730a60542a2d2c4db33b8fd473a455c7a
AdityaWiradarma <79495596+AdityaWiradarma@users.noreply.github.com>
2021-03-07 20:35:18 +0700

    Add files via upload

---
+++ aweb/aweb/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+    <title>Document</title>
+</head>
+
+<body>
+    <section class="header">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#"><img src="asset/img/logo2.jpg.png" alt="" srcset=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto text-right">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="media/about.html">ABOUT US</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="media/contact.html">CONTACT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="media/register.html">REGISTER</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+
+            <div class="row banner">
+                <div class="col-md-6">
+                    <h1> Design Grafis </h1>
+                    <p> Create Foto & Design Project </p>
+                </div>
+                <div class="col-md-6">
+                    <img src="asset/img/exp1.svg" class="img-fluid">
+                </div>
+            </div>
+    </section>
+
+    <!--Artikel-->
+    <section class="artikel">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Artikel</h2>
+                    <img src="asset/img/picture1.jpeg" class="img-fluid">
+                    <p>Jasa yang kami bangun ialah Desain Grafis. Usaha yang kami buat bergerak dalam bidang jasa seni desain ataupun editing foto yang akan kami cetak oleh pihak ketiga dan digunakan sebagai oleh-oleh maupun hadiah. Menawarkan jasa desain grafis, berupa pembuatan desain edit foto, desain poster, desain baju, desain banner, plakat dan masih banyak lagi dengan harga yang pastinya terjangkau sesuai desain dan keinginan customer, yang digunakan sebagai hadiah kepada orang tua, sahabat, pasangan maupun pasangan.</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <!--footer-->
+    <section class="footer">
+        <footer class="bg-dark">
+            <div class="container">
+                <div class="row pt-3">
+                    <div class="col text-center">
+                        <p> | Nyoman Aditya Wiradarma - 2021 | </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </section>
+
+</body>
+
+</html>
